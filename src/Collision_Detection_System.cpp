e67f5c6873f5bb0f658da41169159fb21d9a6496
Joel-Milla <jalejandro.milla@outlook.com>
2025-03-25 19:42:09 +0800

    Extract the loop running of the image for easier analysis.

--- src/Collision_Detection_System.cpp
+++ src/Collision_Detection_System.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/core/types.hpp>
 #include <sstream>
 #include <iomanip>
+#include <string>
 #include <vector>
 #include <cmath>
 #include <opencv2/core.hpp>
@@ -129,52 +130,42 @@ void detectImageKeypoints(const cv::Mat imgGray, const bool visualize_keypoints,
     }
 }
 
-int main(int argc, const char *argv[]) {
-    
-    //* Set up image reading parameters
-    std::string dataPath = "../";
-    std::string imgBasePath = dataPath + "images/";
-    std::string imgPrefix = "KITTI/2011_09_26/image_00/data/000000"; // left camera, color
-    std::string imgPathPrefix = imgBasePath + imgPrefix;
-    std::string imgFileType = ".png";
-
-    int imgStartIndex = 0; // first file index to load (assumes Lidar and camera names have identical naming convention)
-    int imgEndIndex = 9;   // last file index to load
-    int imgFillWidth = 4;  // no. of digits which make up the file index (e.g. img-0001.png)
-
-    //* Set up ring buffer
-    // If you will have thousands of images, you cannot have a single vector that will hold all the images. Overtime will fill memory and slow program.
-    // To solve this, create a data structure to hold limit number of images, and delete the old ones when adding new ones
-    int dataBufferSizeLimit = 2;       // no. of images which are held in memory (ring buffer) at the same time
-    std::deque<DataFrame> dataBuffer; // Dequeue for fast insertion/deletion at the ends of the list
-    bool bVis = false;            // visualize results
-
+/**
+ * @brief Function that loops over the 10 images with the configuration received
+ * 
+ * @param imgStartIndex start of iamge
+ * @param imgEndIndex end index to loop
+ * @param imgFillWidth width of prepending zeros
+ * @param imgPathPrefix path to image
+ * @param imgFileType 'png', etc.
+ * @param dataBufferSizeLimit size limit of buffer
+ * @param visualize_keypoints bool vairable to control visualziation of points
+ * @param visualize_keypoint_matches bool variable to control visualization of keypoint matches
+ * @param detectorType type of detector algorithm [Harris, Fast, Brisk, etc]
+ * @param descriptorType type of descriptor algorithm [Brisk, Brief, Orb, etc]
+ * @param matcherType type of mathcer algorithm [BF, Flann]
+ * @param selectorType type of selector algoirhtm [NN, kNN]
+ * @param bFocusOnVehicle only focus on vehicle upfront
+ * @param dataBuffer unique continer that changes during program, it holds the images, up to limit of dataBufferSizeLimit
+ */
+void loopOverImages(const int imgStartIndex, const int imgEndIndex, const int imgFillWidth, const std::string imgPathPrefix, const std::string imgFileType, const int dataBufferSizeLimit, const bool visualize_keypoints, const bool visualize_keypoint_matches, const DetectorType detectorType, const DescriptorType descriptorType, const MatcherType matcherType, const SelectorType selectorType, const bool bFocusOnVehicle, std::deque<DataFrame> dataBuffer) {
     //* Loop over all imges
     for (size_t imgIndex = 0; imgIndex <= imgEndIndex - imgStartIndex; imgIndex++) {
         //* Main variables used across all the function
         // Variables to extract 2D keypoints from current image
         std::vector<cv::KeyPoint> keypoints;
-        DetectorType detectorType = DetectorType::SIFT;
-        
-        // Variables to keep keypoints on preceding vehicle
-        bool bFocusOnVehicle = true;
-        cv::Rect vehicleRect(535, 180, 180, 150);
         
         bool limitKpts = false; // Only for debugging purposes
         
         // Variables to get descriptors of keypoints
         cv::Mat descriptors;
-        DescriptorType descriptorType = DescriptorType::BRISK; // BRIEF, ORB, AKAZE, SIFT
+
+        // Bounding box of preceding vehicle
+        cv::Rect vehicleRect(535, 180, 180, 150);
 
         // Variables to get matches between two images
         std::vector<cv::DMatch> matches;
-        MatcherType matcherType = MAT_BF;
         bool is_des_binary = isBinaryDescriptor(descriptorType); // DES_BINARY, DES_HOG
-        SelectorType selectorType = SEL_NN;   // SEL_NN, SEL_KNN
-        
-        // Visualization variables
-        bool visualize_keypoints = true;
-        bool visualize_keypoint_matches = false;
 
         //* Start creating and matching keypoints
         std::cout << "Image index: " << imgIndex << std::endl;
@@ -215,6 +206,42 @@ int main(int argc, const char *argv[]) {
         }
         std::cout << std::endl;
     } // eof loop over all images
+}
+
+int main(int argc, const char *argv[]) {
+    
+    //* Set up image reading parameters
+    std::string dataPath = "../";
+    std::string imgBasePath = dataPath + "images/";
+    std::string imgPrefix = "KITTI/2011_09_26/image_00/data/000000"; // left camera, color
+    std::string imgPathPrefix = imgBasePath + imgPrefix;
+    std::string imgFileType = ".png";
+
+    int imgStartIndex = 0; // first file index to load (assumes Lidar and camera names have identical naming convention)
+    int imgEndIndex = 9;   // last file index to load
+    int imgFillWidth = 4;  // no. of digits which make up the file index (e.g. img-0001.png)
+
+    //* Set up ring buffer
+    // If you have thousands of images, you cannot have a single vector that will hold all the images. Overtime will fill memory and slow program.
+    // To solve this, create a data structure to hold limit number of images, and delete the old ones when adding new ones
+    int dataBufferSizeLimit = 2;       // no. of images which are held in memory (ring buffer) at the same time
+
+    // Visualization variables
+    bool visualize_keypoints = true;
+    bool visualize_keypoint_matches = false;
+
+    // Types
+    DetectorType detectorType = DetectorType::SIFT;
+    DescriptorType descriptorType = DescriptorType::BRISK; // BRIEF, ORB, AKAZE, SIFT
+    MatcherType matcherType = MAT_BF;
+    SelectorType selectorType = SEL_NN;   // SEL_NN, SEL_KNN
+
+    // Variables to keep keypoints on preceding vehicle
+    bool bFocusOnVehicle = true;
+
+    //* The lines below are in charge of looping over the program
+    std::deque<DataFrame> dataBuffer; // Dequeue for fast insertion/deletion at the ends of the list
+    loopOverImages(imgStartIndex, imgEndIndex, imgFillWidth, imgPathPrefix, imgFileType, dataBufferSizeLimit, visualize_keypoints, visualize_keypoint_matches, detectorType, descriptorType, matcherType, selectorType, bFocusOnVehicle, dataBuffer);
 
     return 0;
 }
